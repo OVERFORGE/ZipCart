5ec178411315c4c700569c350a81bac9e79283e8
OVERFORGE <dkdakshkhurana@gmail.com>
2025-08-29 02:42:41 +0530

    controllers and middleware done

---
+++ server/src/controllers/product/product.js
@@ -0,0 +1,15 @@
+import { Product } from "../../models";
+
+export const getProductByCategoryId = async (req, res) => {
+  const { categoryId } = req.params;
+
+  try {
+    const products = await Product.find({ category: categoryId })
+      .select("-category")
+      .exec();
+
+    return res.send(products);
+  } catch (error) {
+    return res.status(500).send({ message: "An error occurred ", error });
+  }
+};
---
+++ server/src/routes/auth.js
@@ -0,0 +1,10 @@
+import {
+  fetchUser,
+  loginCustomer,
+  loginDeliveryPartner,
+  refreshToken,
+} from "../controllers/auth/auth";
+
+import { updateUser } from "../controllers/tracking/user";
+
+import {verifyToken }
